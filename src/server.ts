31485ee7843ba59203a9625dcb8710a234725a85
Kumar Piyush <kr.piyush888@gmail.com>
2023-10-08 19:26:13 +0530

    security fix

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ async function fetchAnalytics(): Promise<any> {
     try {
         const response: any = await axios.get('https://intent-kit-16.hasura.app/api/rest/blogs', {
             headers: {
-                'x-hasura-admin-secret': '32qR4KmXOIpsGPQKMqEJHGJS27G5s7HdSKO3gdtQd2kv5e852SiYwWNfxkZOBuQ6'
+                'x-hasura-admin-secret': process.env.HEADER
             }
         });
         const data: Array<any> = response.data.blogs;
